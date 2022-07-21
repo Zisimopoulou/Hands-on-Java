0bf04b484b9d26010ca51181972859767e4560cb
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-07-21 11:54:19 +0300

    Create attributes,getters and setters in the class Order.

---
+++ src/main/java/Order.java
@@ -0,0 +1,25 @@
+public class Order {
+    private String orderID;
+    private Customer customer;
+
+    public Order(String orderID, Customer customer) {
+        this.orderID = orderID;
+        this.customer = customer;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
