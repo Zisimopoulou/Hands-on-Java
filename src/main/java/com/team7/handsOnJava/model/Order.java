2bb58388586d085fb1838c704cbac00e7af6612d
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-07-25 11:18:28 +0300

    Add status attribute to Order class.

--- src/main/java/com/team7/handsOnJava/model/Order.java
+++ src/main/java/com/team7/handsOnJava/model/Order.java
@@ -6,18 +6,25 @@ public class Order {
     private String orderID;
     private Customer customer;
 
-    public Order(String orderID, Customer customer) {
+    private String status;
+
+    public Order(String orderID, String status, Customer customer) {
         this.orderID = orderID;
         this.customer = customer;
+        this.status = status;
     }
 
     public String getOrderID() {
         return orderID;
     }
 
+    public String getStatus() {return status;}
+
     public void setOrderID(String orderID) {
         this.orderID = orderID;
     }
+
+    public void setStatus(String status) {this.status = status;}
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
