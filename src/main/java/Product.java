48b1e77529fd395cbe341403f41de62ebb55b691
d.paliogiannis <oldjohn0000@gmail.com>
2022-07-25 10:45:06 +0300

    Product.java

---
+++ src/main/java/Product.java
@@ -0,0 +1,37 @@
+public class Product {
+
+        private String productID;
+        private String productPrice;
+        private String productName;
+
+
+
+
+        public Product(String productName, String productPrice,String productID) {
+            this.productID = productID;
+            this.productName=productName;
+            this.productPrice=productPrice;
+        }
+
+    public String getproductID() {
+        return productID;
+    }
+    public String getProductPrice() {
+        return productPrice;
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+
+    public void setProductName(productName) {
+        this.productName = productName;
+    }
+    public void setproductID(productID) {
+        this.productID = productID;
+    }
+    public void setProductPrice(productPrice) {
+        this.productPrice = productPrice;
+    }
+
+
