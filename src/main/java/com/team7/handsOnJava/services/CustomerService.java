91cf448bb64d4233d8ef986a6dab5af31d1b8bad
Petros Dhespollari <peterdsp@icloud.com>
2022-07-27 11:56:43 +0300

    Add methods for changes in the Customer

--- src/main/java/com/team7/handsOnJava/services/CustomerService.java
+++ src/main/java/com/team7/handsOnJava/services/CustomerService.java
@@ -1,11 +1,12 @@
 package com.team7.handsOnJava.services;
 
 import com.team7.handsOnJava.exception.EshopException;
-import com.team7.handsOnJava.model.Customer;
+import com.team7.handsOnJava.model.*;
 import com.team7.handsOnJava.repository.CRUDRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public class CustomerService implements CRUDRepository<Customer, String> {
     @Override
@@ -37,4 +38,27 @@ public class CustomerService implements CRUDRepository<Customer, String> {
     public boolean update(Customer customer) throws EshopException {
         return false;
     }
+    //It only checks the presence of the @ symbol in the email address.
+    //If present, then the validation result returns true, otherwise, the result is false
+    public static boolean validEmail(String emailAddress, String regexPattern) {
+            return Pattern.compile(regexPattern)
+                    .matcher(emailAddress)
+                    .matches();
+    }
+    public void changeEmail(Customer customer, String newEmail){
+        customer.setCustomerEmail(newEmail);
+    }
+    public void changeAddress(Customer customer,  CustomerAddress address){
+        customer.setCustomerAddress(address);
+    }
+    public void changeCreditDebitCard(Customer customer,  CreditDebitCard creditDebitCard){
+        customer.getCustomerPaymentMethod().setCreditDebitCard(creditDebitCard);
+    }
+    public void changeCash(Customer customer, Cash cash){
+        customer.getCustomerPaymentMethod().setCash(cash);
+    }
+    public void changeWireTransfer(Customer customer, WireTransfer wireTransfer){
+        customer.getCustomerPaymentMethod().setWireTransfer(wireTransfer);
+    }
+
 }
