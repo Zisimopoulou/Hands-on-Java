6137e340f499e3f00184451d39a9f770f5f86947
Petros Dhespollari <peterdsp@icloud.com>
2022-07-28 13:27:38 +0300

    Update B2cIndividual

--- src/main/java/com/team7/handsOnJava/model/B2cIndividual.java
+++ src/main/java/com/team7/handsOnJava/model/B2cIndividual.java
@@ -3,14 +3,7 @@ package com.team7.handsOnJava.model;
 import java.math.BigDecimal;
 
 public class B2cIndividual extends TypeOfCustomer{
-    public B2cIndividual(String customerID, String customerName, String customerEmail,
-                         CustomerAddress customerAddress, CustomerPaymentMethod customerPaymentMethod,
-                         TypeOfCustomer typeOfCustomer, String typeOfCustomerID, BigDecimal discount) {
-        super(customerID, customerName, customerEmail, customerAddress, customerPaymentMethod,
-                typeOfCustomer, typeOfCustomerID, BigDecimal.valueOf(0.0));
+    public B2cIndividual(String typeOfCustomerID, BigDecimal discount) {
+        super(typeOfCustomerID, BigDecimal.valueOf(0.0));
     }
 }
-
-
-//Μπορω να περασω το ποσοστο εκπτωσης κατευθειαν
-    //μεσα και απλα να καλεις το ποσοστο μεσω της κλασης
