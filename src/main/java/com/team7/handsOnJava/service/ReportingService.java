646583c69dc0a442b9be1c3397eb1335a9c42533
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-07-28 10:13:01 +0300

    Implement ReportingService.java.

--- src/main/java/com/team7/handsOnJava/service/ReportingService.java
+++ src/main/java/com/team7/handsOnJava/service/ReportingService.java
@@ -1,4 +1,33 @@
 package com.team7.handsOnJava.service;
 
+import com.team7.handsOnJava.exception.EshopException;
+import com.team7.handsOnJava.model.Customer;
+import com.team7.handsOnJava.model.Product;
+import com.team7.handsOnJava.repository.CustomerRepository;
+import com.team7.handsOnJava.repository.OrderRepository;
+import com.team7.handsOnJava.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Map;
+@Slf4j
 public class ReportingService {
+    private OrderRepository orderRepository;
+    private CustomerRepository customerRepository;
+    private ProductRepository productRepository;
+
+    public Map<Long, ArrayList<Long>> findTotNumAndCostOfPurchasesProduct(Product product) throws EshopException {
+        log.info("Finding total number and cost of purchases for the product with ID = {}",product.getId());
+        return orderRepository.findTotNumAndCostOfPurchasesProduct(product);
+    }
+
+    public Map<Long, ArrayList<Long>> findTotNumAndCostOfPurchasesProduct(Customer customer) throws EshopException {
+        log.info("Finding total number and cost of purchases for the product with ID = {}",customer.getId());
+        return customerRepository.findTotNumAndCostOfPurchasesCustomer(customer);
+    }
+    public Map<Long, ArrayList<Long>> findTotNumAndCostOfPurchasesPerCustomer() throws EshopException {
+        log.info("Finding total number and cost of purchases per customer.");
+        return customerRepository.findTotNumAndCostOfPurchasesPerCustomer();
+    }
+
 }
