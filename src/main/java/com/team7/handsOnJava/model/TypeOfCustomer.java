fcb40ff156fe2f63364e86cb5d7f37e500b9b683
Petros Dhespollari <peterdsp@icloud.com>
2022-07-28 13:28:26 +0300

    Update TypeOfCustomer

--- src/main/java/com/team7/handsOnJava/model/TypeOfCustomer.java
+++ src/main/java/com/team7/handsOnJava/model/TypeOfCustomer.java
@@ -2,15 +2,11 @@ package com.team7.handsOnJava.model;
 
 import java.math.BigDecimal;
 
-public class TypeOfCustomer extends Customer{
-    private String typeOfCustomerID;
+public class TypeOfCustomer extends BaseModel{
     private BigDecimal discount;
 
-    public TypeOfCustomer(String customerID, String customerName, String customerEmail,
-                          CustomerAddress customerAddress, CustomerPaymentMethod customerPaymentMethod,
-                          TypeOfCustomer typeOfCustomer, String typeOfCustomerID, BigDecimal discount) {
-        super(customerID, customerName, customerEmail, customerAddress, customerPaymentMethod, typeOfCustomer);
-        this.typeOfCustomerID= typeOfCustomerID;
+    public TypeOfCustomer(String typeOfCustomerID, BigDecimal discount) {
+        super(typeOfCustomerID);
         this.discount= discount;
     }
 }
