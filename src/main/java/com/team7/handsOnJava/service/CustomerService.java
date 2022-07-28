ff8ed5290920d5e66f65c296389887ddbb2dfc81
Petros Dhespollari <peterdsp@icloud.com>
2022-07-28 05:48:03 +0300

    Create new interface CustomerService for the statements of the methods bodies in CustomerServiceImpl

---
+++ src/main/java/com/team7/handsOnJava/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.team7.handsOnJava.service;
+
+import com.team7.handsOnJava.model.*;
+
+import java.util.regex.Pattern;
+
+public interface CustomerService {
+    //It only checks the presence of the @ symbol in the email address.
+    //If present, then the validation result returns true, otherwise, the result is false
+    public static boolean validEmail(String emailAddress, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(emailAddress)
+                .matches();
+    }
+    public void changeEmail(Customer customer, String newEmail);
+    public void changeAddress(Customer customer,  CustomerAddress address);
+    public void changeCreditDebitCard(Customer customer,  CreditDebitCard creditDebitCard);
+    public void changeCash(Customer customer, Cash cash);
+    public void changeWireTransfer(Customer customer, WireTransfer wireTransfer);
+}
