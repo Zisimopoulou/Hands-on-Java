5230afd4c956ad64aaaabc3f7a37af4608b14a19
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-07-28 10:56:42 +0300

    Move base service to service class.

---
+++ src/main/java/com/team7/handsOnJava/service/BaseService.java
@@ -0,0 +1,24 @@
+package com.team7.handsOnJava.service;
+
+import com.team7.handsOnJava.exception.EshopException;
+import com.team7.handsOnJava.model.BaseModel;
+
+import java.util.List;
+
+public interface BaseService<T extends BaseModel> {
+    T create(final T entity) throws EshopException;
+
+    List<T> createAll(final T... entities) throws EshopException;
+
+    List<T> createAll(final List<T> entities) throws EshopException;
+
+    void delete(T entity) throws EshopException;
+
+    void deleteById(Long id) throws EshopException;
+
+    boolean exists(T entity) throws EshopException;
+
+    T get(Long id) throws EshopException;
+
+    List<T> findAll() throws EshopException;
+}
