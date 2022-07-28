ebfa00bf1064421ceb87533546079062581de753
Petros Dhespollari <peterdsp@icloud.com>
2022-07-28 12:20:23 +0300

    Update CustomerPaymentMethod

--- src/main/java/com/team7/handsOnJava/model/CustomerPaymentMethod.java
+++ src/main/java/com/team7/handsOnJava/model/CustomerPaymentMethod.java
@@ -4,21 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.math.BigDecimal;
-
 @Getter
 @Setter
 @ToString
 public class CustomerPaymentMethod extends BaseModel{
-    private BigDecimal discount;
     private CreditDebitCard creditDebitCard;
     private WireTransfer wireTransfer;
     private Cash cash;
-    public CustomerPaymentMethod(String paymentMethodID, BigDecimal discount) {
+    public CustomerPaymentMethod(String paymentMethodID) {
         super(paymentMethodID);
         this.creditDebitCard= creditDebitCard;
         this.wireTransfer= wireTransfer;
         this.cash= cash;
-        this.discount= discount;
     }
 }
