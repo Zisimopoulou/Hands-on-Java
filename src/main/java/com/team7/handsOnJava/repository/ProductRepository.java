5300ceec1b1284a256ef457b2f5afc31e825e8d8
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-07-29 02:49:01 +0300

    Change location of product report.

--- src/main/java/com/team7/handsOnJava/repository/ProductRepository.java
+++ src/main/java/com/team7/handsOnJava/repository/ProductRepository.java
@@ -139,32 +139,28 @@ public class ProductRepository implements CRUDRepository<Product, String> {
         }
 
 
-        public Map<Long, ArrayList<Long>> findTotNumAndCostOfPurchasesProduct (Product product) throws EshopException {
-            try (Connection connection = DataSource.getConnection();
-                 PreparedStatement preparedstatement = connection.prepareStatement(
-                         SqlCommandRepository.get("select.report.orderitem.000"))) {
+    public Map<Long, ArrayList<Long>> findTotNumAndCostOfPurchasesProduct(Product product) throws EshopException {
+        try(Connection connection = DataSource.getConnection();
+            PreparedStatement preparedstatement = connection.prepareStatement(
+                    SqlCommandRepository.get("select.report.product.000"))) {
 
-                log.debug("Finding total number and cost of purchases for a particular product with order ID=?????????");
+            log.debug("Finding total number and cost of purchases for a particular product with product ID={}",product.getId());
 
-                preparedstatement.setString(1, product.getId());
+            preparedstatement.setString(1, product.getId());
 
-                ResultSet resultSet = preparedstatement.executeQuery();
-                final HashMap<Long, ArrayList<Long>> totNumAndCostOfPurchasesProduct = new HashMap<>();
-                while (resultSet.next()) {
-                    ArrayList<Long> list = new ArrayList<>();
-                    list.add(resultSet.getLong("total_quantity"));
-                    list.add(resultSet.getLong("total_price"));
-                    totNumAndCostOfPurchasesProduct.put(resultSet.getLong("product_id"), list);
-                    list.remove(0);
-                    list.remove(1);
-                }
+            ResultSet resultSet = preparedstatement.executeQuery();
+            final HashMap<Long, ArrayList<Long>> totNumAndCostOfPurchasesProduct = new HashMap<>();
+            ArrayList<Long> list = new ArrayList<>();
+            list.add(resultSet.getLong("total_quantity"));
+            list.add(resultSet.getLong("total_price"));
+            totNumAndCostOfPurchasesProduct.put(resultSet.getLong("product_id"),list);
 
-                return totNumAndCostOfPurchasesProduct;
+            return totNumAndCostOfPurchasesProduct;
 
-            } catch (SQLException e) {
-                throw new EshopException("Could not find total number or cost of purchases for product", e);
-            }
+        } catch (SQLException e) {
+            throw new EshopException("Could not find total number or cost of purchases for product",e);
         }
+    }
     }
 
 
