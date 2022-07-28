c5b7df3577298225de772fb5377ab6986991d1b5
Petros Dhespollari <peterdsp@icloud.com>
2022-07-28 13:27:06 +0300

    Update B2bBusiness

--- src/main/java/com/team7/handsOnJava/model/B2bBusiness.java
+++ src/main/java/com/team7/handsOnJava/model/B2bBusiness.java
@@ -3,10 +3,7 @@ package com.team7.handsOnJava.model;
 import java.math.BigDecimal;
 
 public class B2bBusiness extends TypeOfCustomer{
-    public B2bBusiness(String customerID, String customerName, String customerEmail,
-                         CustomerAddress customerAddress, CustomerPaymentMethod customerPaymentMethod,
-                         TypeOfCustomer typeOfCustomer, String typeOfCustomerID, BigDecimal discount) {
-        super(customerID, customerName, customerEmail, customerAddress, customerPaymentMethod,
-                typeOfCustomer, typeOfCustomerID, BigDecimal.valueOf(0.2));
+    public B2bBusiness(String typeOfCustomerID, BigDecimal discount) {
+        super(typeOfCustomerID, BigDecimal.valueOf(0.2));
     }
 }
