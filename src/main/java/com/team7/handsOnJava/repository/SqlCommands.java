11bc3cb39290c2653b0ee32b1fcf84287cf024a9
a.zisimopoulou <zisimopouloua@protonmail.com>
2022-07-31 04:09:28 +0300

    Correct SqlCommands.java file.

--- src/main/java/com/team7/handsOnJava/repository/SqlCommands.java
+++ src/main/java/com/team7/handsOnJava/repository/SqlCommands.java
@@ -1,8 +1,28 @@
 package com.team7.handsOnJava.repository;
-import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import static java.lang.System.exit;
+@Slf4j
 public class SqlCommands {
     private static final Properties sqlCommands = new Properties();
+
+    static {
+        try (InputStream inputStream = SqlCommands.class.getClassLoader().getResourceAsStream(
+                "sql.properties")) {
+            if (inputStream == null) {
+                log.error("Sorry, unable to find sql.properties, exiting application.");
+                exit(-1);
+            }
+
+            sqlCommands.load(inputStream);
+        } catch (IOException ex) {
+            log.error("Sorry, unable to parse sql.properties, exiting application.", ex);
+            exit(-1);
+        }
+    }
 
     private SqlCommands() {
     }
