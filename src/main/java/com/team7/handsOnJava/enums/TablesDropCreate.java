3ef540e995506a34771c90827cab277b4f6de348
Petros Dhespollari <peterdsp@icloud.com>
2022-08-02 16:32:31 +0300

    Update TablesDropCreate with (CUSTOMERADDRESSDROP, CUSTOMERTYPEOFDROP, CUSTOMERADDRESSCREATE, CUSTOMERTYPEOFCREATE)

--- src/main/java/com/team7/handsOnJava/enums/TablesDropCreate.java
+++ src/main/java/com/team7/handsOnJava/enums/TablesDropCreate.java
@@ -7,9 +7,13 @@ public enum TablesDropCreate {
     ORDERITEMDROP("drop","drop.table.orderitem"),
     ORDERDROP("drop","drop.table.orders"),
     PRODUCTDROP("drop","drop.table.product"),
+    CUSTOMERADDRESSDROP("drop","drop.table.customeraddress"),
+    CUSTOMERTYPEOFDROP("drop","drop.table.typeofcustomer"),
     CUSTOMERDROP("drop","drop.table.customer"),
     PRODUCTCREATE("create","create.table.product"),
     CUSTOMERCREATE("create","create.table.customer"),
+    CUSTOMERADDRESSCREATE("create","create.table.customeraddress"),
+    CUSTOMERTYPEOFCREATE("create","create.table.typeofcustomer"),
     ORDERCREATE("create","create.table.orders"),
     ORDERITEMCREAT("create","create.table.orderitem");
 
